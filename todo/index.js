8363ba136172b0e89396a85186d945ee44d71777
Rahul Kumar <67073807+Rahul-Chahar@users.noreply.github.com>
2024-04-16 18:58:22 +0000

    Task Analyser

---
+++ todo/index.js
@@ -0,0 +1,18 @@
+/* Task analyser */
+
+const todo =[
+    {taskName: "Reading newspaper", status: false},
+    {taskName: "Taking revison class", status: true},
+    {taskName: "Completing feature 01", status: false},
+]
+
+function taskAnalyser(todo){
+    todo.forEach((data)=>{
+        if(data.status){
+            console.log(`task ${data.taskName} is completed`);
+        } else {
+            console.log(`task ${data.taskName} is pending`);
+        }
+    })
+}
+taskAnalyser(todo)
